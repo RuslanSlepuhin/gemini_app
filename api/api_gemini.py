65423812d676dba54f04165cd830f7cc314c7109
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2024-03-11 09:18:29 +0300

    api

--- api/api_gemini.py
+++ api/api_gemini.py
@@ -10,7 +10,7 @@ CORS(app)
 @app.route("/gemini_request", methods=["POST"])
 def gemini_request():
     question = request.json['request']
-    print(question)
+    print(question, type(question))
     # answer = gemini_ai(question)
     return {'answer': "hegay"}
 
