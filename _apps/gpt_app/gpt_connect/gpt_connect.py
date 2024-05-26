9c5b87acc021f4aa6e0c568863c48e78db490d91
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2024-05-26 23:59:36 +0300

    gpt

--- _apps/gpt_app/gpt_connect/gpt_connect.py
+++ _apps/gpt_app/gpt_connect/gpt_connect.py
@@ -11,6 +11,7 @@ def available_models():
     models = openai.Model.list()
     for model in models['data']:
         print(model['id'])
+    return models['data']
 
 
 def get_gpt4_response(prompt, model="davinci-002",  temperature=0.7, max_tokens=150):
