b23e7ecb21d9a79a426b80c9b2f4946d470251ef
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2024-05-26 19:01:27 +0300

    gpt

--- _apps/gemini_app/api/api_gemini.py
+++ _apps/gemini_app/api/api_gemini.py
@@ -3,6 +3,7 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 from flask import request
 from _apps.gemini_app.geminitrial.views import gemini_ai
+from _apps.gpt_app.gpt_connect.gpt_connect import get_gpt4_response
 
 app = Flask(__name__)
 CORS(app)
@@ -11,6 +12,12 @@ CORS(app)
 async def gemini_request():
     question = request.json['request']
     answer = await gemini_ai(question)
+    return jsonify({'answer': answer})
+
+@app.route("/gpt_request", methods=["POST"])
+async def gpt_request():
+    question = request.json['request']
+    answer = get_gpt4_response(question)
     return jsonify({'answer': answer})
 
 app.run(host="127.0.0.1", port=int(os.environ.get('PORT', 5000)))
