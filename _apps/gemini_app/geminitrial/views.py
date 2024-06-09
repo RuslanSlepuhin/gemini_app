bf7b27d88a3c9b7a5d0d5cf68246a616c15c1a9e
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2024-06-09 23:37:20 +0300

    Gemini response

--- _apps/gemini_app/geminitrial/views.py
+++ _apps/gemini_app/geminitrial/views.py
@@ -4,4 +4,5 @@ from _apps.gemini_app.geminitrial.models import model
 async def gemini_ai(prompt):
     prompt = prompt
     response = model.generate_content(contents=[prompt])
+    print(f"\033[1;33m{response}\033[0m")
     return response.text
