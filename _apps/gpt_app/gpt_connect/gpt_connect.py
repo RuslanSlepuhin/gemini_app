5dc8627af5df2a31d71f1e19b91f0bfa4edee0bc
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2024-12-23 18:26:14 +0300

    gpt-4o updated

--- _apps/gpt_app/gpt_connect/gpt_connect.py
+++ _apps/gpt_app/gpt_connect/gpt_connect.py
@@ -1,10 +1,12 @@
 import os
 import openai
 from dotenv import load_dotenv
+import logging
 
 load_dotenv()
 openai_api_key = os.getenv("OPENAI_API_KEY")
 openai.api_key = openai_api_key
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 def available_models():
@@ -30,6 +32,43 @@ def get_gpt4_response(prompt, model="gpt-4o", temperature=0.7, max_tokens=150, *
         timeout=15,
     )
     return response.choices[0].message['content'].strip()
+
+def get_gpt4_response_updated(prompt, model="gpt-4", temperature=0.7, max_tokens=150, **kwargs):
+    if not openai.api_key:
+        logging.error("API key for OpenAI is not set.")
+        raise ValueError("API key for OpenAI is not set.")
+
+    # Определение системной роли
+    incorrect_picture_status = "Невозможно восстановить текст."
+    role_system_content = kwargs.get(
+        'role_system_content',
+        f"Это некорректно распознанный OCR чек из магазина. Восстанови его и пришли в формате: "
+        f"штрих-код продукта; продукт; единица измерения; цена; количество; стоимость. "
+        f"Каждый следующий продукт с новой строки. Ответь без лишнего текста, только по сути. "
+        f"Если текст невозможно восстановить, выдай ответ: {incorrect_picture_status}"
+    )
+
+    logging.debug(f"!! PROMPT: {prompt}")
+    logging.debug(f"!! ROLE_SYSTEM_CONTENT: {role_system_content}")
+
+    try:
+        response = openai.chat.completions.create(
+            model=model,
+            messages=[
+                {"role": "system", "content": role_system_content},
+                {"role": "user", "content": prompt}
+            ],
+            temperature=temperature,
+            max_tokens=max_tokens,
+            n=1,
+            stop=None,
+            timeout=15,
+        )
+        logging.debug(f"!! RESPONSE: {response}")
+        return response.choices[0].message.content.strip()
+    except openai.OpenAIError as e:
+        logging.error(f"An error occurred: {e}")
+        return None
 
 
 if __name__ == "__main__":
